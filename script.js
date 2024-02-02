bf4dfb0470c1ca9a54c66510a077287948508b2d
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-02-03 00:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
+
 function volume_sphere() {
-    //Write your code here
-  
+    let radiusEle=document.getElementById("radius");
+	let volEle=document.getElementById("volume")
+	let r=radiusEle.value;
+	let volume=(4*r*r*r)/3;
+	volEle.value=volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
