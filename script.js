eb7f517767d084b0333206ffb435d302244b7160
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-02-03 00:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function volume_sphere(e) {
 	e.preventDefault();
     const form=document.getElementById("MyForm");
 	let r=form.radius.value;
-	form.volume.value=(4*Math.PI*r**3)/3;
+	let v=(4*Math.PI*r**3)/3;
+	v=Math.round(v*10000)/10000;
+	form.volume.value=v;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
